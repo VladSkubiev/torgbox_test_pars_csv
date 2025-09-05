5446822488ddce06c2c1270c2bd9e3a93430e51a
Git adm <vlad-skubiev3@mail.ru>
2025-09-05 22:41:17 +0700

    download and safe name

--- test_curl.sh
+++ test_curl.sh
@@ -6,7 +6,13 @@ curl -s https://fsa.gov.ru/opendata/7736638268-rss/ > /torgbox/link.txt
 link=$(grep -A2 "Гиперссылка" /torgbox/link.txt | grep https | cut -d '"' -f 2)
 echo $link
 rm -f /torgbox/link.txt
-curl -so /torgbox/ftp_data/FSA_data/declaration/original/archive.7z  $link
+cd /torgbox/ftp_data/FSA_data/declaration/original/ && curl -O $link  
+
+#curl -s https://fsa.gov.ru/opendata/7736638268-rds/ > /torgbox/link.txt
+#link=$(grep -A2 "Гиперссылка" /torgbox/link.txt | grep https | cut -d '"' -f 2)
+#echo $link
+#rm -f /torgbox/link.txt
+#curl -so /torgbox/ftp_data/FSA_data/certificate/original/archive.7z  $link
 #link=cat /home/user/Рабочий\ стол/link1.txt | grep Гиперссылка -A2
 
 #echo | $link | grep -A2 Гиперссылка
